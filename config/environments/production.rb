6b3d1af1b8c5e12481a6710d0c88ab43daee2b23
Anna <a.dimitrova.au@gmail.com>
2017-05-03 14:51:43 +1000

    chaged production file before deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,9 @@ config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
